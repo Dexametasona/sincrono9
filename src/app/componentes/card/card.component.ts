10b749608d479b1b365db809c98ce68d021ed5fb
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-03 10:59:03 -0500

    estilos finales

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  estado=false;
+  estado=true;
 
   mostrar(){
     this.estado=!this.estado;
